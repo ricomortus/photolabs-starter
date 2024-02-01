c53a2e737b35b51d3f64afb33b651361cbdf3b5b
Rico Chow <rico.ck.chow@gmail.com>
2024-02-01 08:34:09 -0500

    Add blank modal to PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import PhotoFavButton from "./PhotoFavButton";
+import PhotoDetailsModel from "../routes/PhotoDetailsModal";
 import '../App.scss';
 import '../styles/PhotoListItem.scss'
 
@@ -7,11 +8,20 @@ const PhotoListItem = (props) => {
   //Destructure photo after, toggleFavorite function which passes in the photo id as its argument, and isFavorite which checks whether the favoritePhotos array includes the current photo and returns a boolean
   const { photo, toggleFavorite, isFavorite } = props;
 
+  //Create state management for toggling modal
+  const [ isModalOpen, setModalOpen ] = useState(false);
+
+  //Create function to toggle modal
+  const toggleModal = () => {
+    setModalOpen(!isModalOpen);
+  };
+
   return (
-    <div className="photo-list__item"> 
+    <div className="photo-list__item"  onClick={toggleModal}> 
       {/**Pass the toggleFavorite function to the onClick event and the boolean value of isFavorite */}
       <PhotoFavButton onClick={toggleFavorite} isFavorite={isFavorite}/>
-      <img src={photo.urls.regular}className="photo-list__image" />
+      <img src={photo.urls.regular} className="photo-list__image"/>
+      { isModalOpen && < PhotoDetailsModel/> }
       <div className="photo-list__user-info">
         <img src={photo.user.profile}className="photo-list__user-profile"/>
         <p>{photo.user.name}</p>
